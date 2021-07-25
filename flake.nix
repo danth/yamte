5c9cf59b436ef65ee23f63bb3fb032397a13aeae
Daniel Thwaites <danthwaites30@btinternet.com>
2021-07-25 11:26:04 +0100

    Initial commit :tada:

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.yamte = pkgs.stdenv.mkDerivation {
+          name = "yamte";
+          src = ./.;
+          buildPhase = "cc yamte.c -o yamte -Wall -Wextra -pedantic -std=c99";
+          installPhase = "install -D yamte $out/bin/yamte";
+        };
+        defaultPackage = packages.yamte;
+
+        apps.yamte = utils.lib.mkApp { drv = packages.yamte; };
+        defaultApp = apps.yamte;
+
+        checks = packages;
+      });
+}
