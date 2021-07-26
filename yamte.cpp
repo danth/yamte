59cf92ffd310607f7ad9ba9b7dcd99036c89c84f
Daniel Thwaites <danthwaites30@btinternet.com>
2021-07-26 20:10:38 +0100

    Begin converting other code to OOP :construction:

--- yamte.cpp
+++ yamte.cpp
@@ -7,8 +7,6 @@
 #define TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-/*** state ***/
-
 class Row {
   private:
     std::string text;
@@ -58,12 +56,14 @@ class Row {
 
     void insertCharacter(int at, int character) {
       if (at < 0 || at > text.size()) at = text.size();
+
       text.insert(text.begin() + at, character);
       render();
     }
 
     void deleteCharacter(int at) {
       if (at < 0 || at >= text.size()) return;
+
       text.erase(text.begin() + at);
       render();
     }
@@ -80,279 +80,335 @@ class Row {
     }
 };
 
-struct editorState {
-  int editor_lines;
-  int cursor_row, cursor_column, cursor_rendered_column;
-  int row_offset, column_offset;
-  std::string filename;
-  std::vector <Row> rows;
+class Editor;
+
+class Cursor {
+  private:
+    int cursor_row;
+    int cursor_column;
+
+  public:
+    Cursor() {
+      cursor_row = 0;
+      cursor_column = 0;
+    }
+
+    int getRow() {
+      return cursor_row;
+    }
+
+    void setRow(int row) {
+      cursor_row = row;
+    }
+
+    void adjustRow(int by) {
+      cursor_row += by;
+    }
+
+    int getColumn() {
+      return cursor_column;
+    }
+
+    void setColumn(int column) {
+      cursor_column = column;
+    }
+
+    void adjustColumn(int by) {
+      cursor_column += by;
+    }
+
+    void move(int key, Editor *editor) {
+      switch (key) {
+        case KEY_UP:
+          if (cursor_row > 0) cursor_row--;
+          break;
+        case KEY_DOWN:
+          // Move down unless we are on the last line
+          if (cursor_row < editor->countRows()) cursor_row++;
+          break;
+        case KEY_LEFT:
+          // Move left unless we are at the first column
+          if (cursor_column > 0) cursor_column--;
+          // Go to the last column of the previous line
+          else if (cursor_row > 0) {
+            cursor_row--;
+            cursor_column = editor->getRow(cursor_row).size();
+          }
+          break;
+        case KEY_RIGHT:
+          if (cursor_row < editor->countRows()) {
+            Row row = editor->getRow(cursor_row);
+            // Move right unless we are at the last column
+            if (cursor_column < row.size()) cursor_column++;
+            // Go to the first column of the next line
+            else if (cursor_column == row.size()) {
+              cursor_row++;
+              cursor_column = 0;
+            }
+          }
+          break;
+        case KEY_HOME:
+          // Move to the first column
+          cursor_column = 0;
+          break;
+        case KEY_END:
+          // Move to the right of the screen
+          if (cursor_row < editor->countRows()) {
+            cursor_column = editor->getRow(cursor_row).size();
+          }
+          break;
+      }
+
+      if (cursor_row > editor->countRows()) {
+        cursor_column = 0;
+      } else {
+        int row_length = editor->getRow(cursor_row).size();
+        if (cursor_column > row_length) cursor_column = row_length;
+      }
+    }
 };
-struct editorState state;
 
-void initialiseState() {
-  state.cursor_row = 0;
-  state.cursor_column = 0;
-  state.cursor_rendered_column = 0;
-  state.row_offset = 0;
-  state.column_offset = 0;
-  state.filename = "";
-}
+class Editor {
+  private:
+    Cursor cursor;
+    std::string filename;
+    std::vector<Row> rows;
 
-/*** rows ***/
-
-void rowInsert(int at, std::string text) {
-  if (at < 0 || at > state.rows.size()) return;
-  Row row;
-  row.setText(text);
-  state.rows.insert(state.rows.begin() + at, row);
-}
-
-void rowDelete(int at) {
-  if (at < 0 || at > state.rows.size()) return;
-  state.rows.erase(state.rows.begin() + at);
-}
-
-/*** editor operations ***/
-
-void insertCharacter(int character) {
-  if (state.cursor_row == state.rows.size()) {
-    rowInsert(state.rows.size(), "");
-  }
-  state.rows[state.cursor_row].insertCharacter(state.cursor_column, character);
-  state.cursor_column++;
-}
-
-void deleteCharacter() {
-  if (state.cursor_row == state.rows.size()) return;
-  if (state.cursor_column == 0 && state.cursor_row == 0) return;
-
-  Row *row = &state.rows[state.cursor_row];
-  if (state.cursor_column > 0) {
-    row->deleteCharacter(state.cursor_column - 1);
-    state.cursor_column--;
-  } else {
-    Row *previous_row = &state.rows[state.cursor_row - 1];
-    state.cursor_column = previous_row->size();
-    previous_row->appendText(row->getText());
-    rowDelete(state.cursor_row);
-    state.cursor_row--;
-  }
-}
-
-void insertNewline() {
-  if (state.cursor_column == 0) {
-    rowInsert(state.cursor_row, "");
-  } else {
-    Row *row = &state.rows[state.cursor_row];
-    rowInsert(state.cursor_row + 1, row->getText().substr(state.cursor_column));
-    row = &state.rows[state.cursor_row];
-    row->resizeText(state.cursor_column);
-  }
-  state.cursor_row++;
-  state.cursor_column = 0;
-}
-
-/*** files ***/
-
-void openFile(std::string filename) {
-  state.filename = filename;
-
-  std::fstream file;
-  file.open(filename, std::ios::in);
-  if (file.is_open()) {
-    std::string line;
-    while(getline(file, line)) {
-      rowInsert(state.rows.size(), line);
+  public:
+    Editor() {
+      filename = "";
     }
-    file.close();
-  }
-}
 
-/*** output ***/
-
-void initialiseScreen() {
-  initscr();
-  raw(); // Disable line buffering
-  noecho(); // Don't echo typed characters
-  nonl(); // Disable translation of \r into \n
-  intrflush(stdscr, FALSE);
-  keypad(stdscr, TRUE); // Replace F1, F2, F3... with token values
-}
-
-void clampScroll() {
-  state.cursor_rendered_column = 0;
-  if (state.cursor_row < state.rows.size()) {
-    state.cursor_rendered_column =
-      state.rows[state.cursor_row].renderedColumn(state.cursor_column);
-  }
-
-  // Vertical
-  if (state.cursor_row < state.row_offset) {
-    state.row_offset = state.cursor_row;
-  }
-  if (state.cursor_row >= state.row_offset + state.editor_lines) {
-    state.row_offset = state.cursor_row - state.editor_lines + 1;
-  }
-
-  // Horizontal
-  if (state.cursor_rendered_column < state.column_offset) {
-    state.column_offset = state.cursor_rendered_column;
-  }
-  if (state.cursor_rendered_column >= state.column_offset + COLS) {
-    state.column_offset = state.cursor_rendered_column - COLS + 1;
-  }
-}
-
-void drawRows() {
-  int screen_row;
-  for (screen_row = 0; screen_row < state.editor_lines; screen_row++) {
-    int file_row = screen_row + state.row_offset;
-    if (file_row >= state.rows.size()) {
-      mvaddch(screen_row, 0, '~');
-    } else {
-      std::string rendered = state.rows[file_row].getRendered();
-      std::string visible_rendered = rendered.substr(
-          state.column_offset, state.column_offset + COLS);
-      mvaddstr(screen_row, 0, visible_rendered.c_str());
+    std::string getFilename() {
+      return filename;
     }
-  }
-}
 
-void drawStatus() {
-  attron(A_STANDOUT);
+    bool isFileOpen() {
+      return filename.size();
+    }
 
-  // Fill with spaces to create background
-  int j;
-  for (j = 0; j < COLS; j++) mvaddch(LINES-1, j, ' ');
+    Cursor getCursor() {
+      return cursor;
+    }
 
-  // Overwrite some of the spaces with the status
-  mvprintw(LINES-1, 0, "%.40s - %d lines",
-    state.filename.size() ? state.filename.c_str() : "[no name]", state.rows.size());
+    int countRows() {
+      return rows.size();
+    }
 
-  attroff(A_STANDOUT);
-}
+    Row* getRow(int at) {
+      return &rows[at];
+    }
 
-void refreshScreen() {
-  clear();
-  state.editor_lines = LINES - 1;
-  clampScroll();
-  drawRows();
-  drawStatus();
-  move(
-    state.cursor_row - state.row_offset,
-    state.cursor_rendered_column - state.column_offset
-  );
-  refresh();
-}
+    Row* getCurrentRow() {
+      return getRow(cursor.getRow());
+    }
 
-/*** input ***/
+    int getRenderedColumn() {
+      return getCurrentRow().renderedColumn(cursor.getColumn());
+    }
 
-void moveCursor(int key) {
-  switch (key) {
-    case KEY_UP:
-      // Move up unless we are on the first line
-      if (state.cursor_row > 0) {
-        state.cursor_row--;
+    void insertRow(int at, std::string text) {
+      if (at < 0 || at > rows.size()) return;
+
+      Row row;
+      row.setText(text);
+      rows.insert(rows.begin() + at, row);
+    }
+
+    void deleteRow(int at) {
+      if (at < 0 || at > rows.size()) return;
+
+      rows.erase(rows.begin() + at);
+    }
+
+    void insertCharacter(char character) {
+      if (cursor.getRow() == rows.size()) insertRow(rows.size(), "");
+
+      getCurrentRow().insertCharacter(cursor.getColumn(), character);
+      cursor.adjustColumn(1);
+    }
+
+    void deleteCharacter() {
+      if (cursor.getRow() == rows.size()) return;
+      if (cursor.getColumn() == 0 && cursor.getRow() == 0) return;
+
+      Row *row = &rows[cursor.getRow()];
+      if (cursor.getColumn() > 0) {
+        row->deleteCharacter(cursor.getColumn() - 1);
+        cursor.adjustColumn(-1);
+      } else {
+        Row *previous_row = getRow(cursor.getRow() - 1);
+        cursor.setColumn(previous_row->size());
+        previous_row->appendText(row->getText());
+        deleteRow(cursor.getRow());
+        cursor.adjustRow(-1);
       }
-      break;
-    case KEY_DOWN:
-      // Move down unless we are on the last line
-      if (state.cursor_row < state.rows.size()) {
-        state.cursor_row++;
+    }
+
+    void insertNewline() {
+      if (cursor.getColumn() == 0) {
+        insertRow(cursor.getRow(), "");
+      } else {
+        insertRow(cursor.getRow() + 1, getCurrentRow()->getText().substr(cursor.getColumn()));
+        getCurrentRow()->resizeText(cursor.getColumn());
       }
-      break;
-    case KEY_LEFT:
-      // Move left unless we are at the first column
-      if (state.cursor_column > 0) {
-        state.cursor_column--;
-      }
-      // Go to the last column of the previous line
-      else if (state.cursor_row > 0) {
-        state.cursor_row--;
-        state.cursor_column = state.rows[state.cursor_row].size();
-      }
-      break;
-    case KEY_RIGHT:
-      if (state.cursor_row < state.rows.size()) {
-        Row row = state.rows[state.cursor_row];
-        // Move right unless we are at the last column
-        if (state.cursor_column < row.size()) {
-          state.cursor_column++;
+
+      cursor.adjustRow(1);
+      cursor.setColumn(0);
+    }
+
+    void openFile(std::string f) {
+      filename = f;
+
+      std::fstream file;
+      file.open(filename, std::ios::in);
+      if (file.is_open()) {
+        std::string line;
+        while(getline(file, line)) {
+          insertRow(rows.size(), line);
         }
-        // Go to the first column of the next line
-        else if (state.cursor_column == row.size()) {
-          state.cursor_row++;
-          state.cursor_column = 0;
+        file.close();
+      }
+    }
+
+    void processKey(int key) {
+      switch (key) {
+        case CTRL_KEY('q'):
+          endwin();
+          exit(0);
+          break;
+
+        case KEY_UP:
+        case KEY_DOWN:
+        case KEY_LEFT:
+        case KEY_RIGHT:
+        case KEY_HOME:
+        case KEY_END:
+          cursor.move(key, this);
+          break;
+
+        case '\r':
+          insertNewline();
+          break;
+
+        case KEY_BACKSPACE:
+        case KEY_DC:
+        case CTRL_KEY('h'):
+          if (key == KEY_DC) cursor.move(KEY_RIGHT, this);
+          deleteCharacter();
+          break;
+
+        default:
+          insertCharacter(key);
+          break;
+      }
+    }
+};
+
+class Display {
+  private:
+    Editor editor;
+    int lines;
+    int row_offset;
+    int column_offset;
+
+  public:
+    Display(Editor e) {
+      editor = e;
+      lines = LINES - 1;
+      row_offset = 0;
+      column_offset = 0;
+    }
+
+    void initialiseScreen() {
+      initscr();
+      raw(); // Disable line buffering
+      noecho(); // Don't echo typed characters
+      nonl(); // Disable translation of \r into \n
+      intrflush(stdscr, FALSE);
+      keypad(stdscr, TRUE); // Replace F1, F2, F3... with token values
+    }
+
+    void clampScroll() {
+      // Vertical
+      if (editor.getCursor().getRow() < row_offset) {
+        row_offset = editor.getCursor().getRow();
+      }
+      if (editor.getCursor().getRow() >= row_offset + lines) {
+        row_offset = editor.getCursor().getRow() - lines + 1;
+      }
+
+      // Horizontal
+      if (editor.getRenderedColumn() < column_offset) {
+        column_offset = editor.getRenderedColumn();
+      }
+      if (editor.getRenderedColumn() >= column_offset + COLS) {
+        column_offset = editor.getRenderedColumn() - COLS + 1;
+      }
+    }
+
+    void drawRows() {
+      int screen_row;
+      for (screen_row = 0; screen_row < lines; screen_row++) {
+        int file_row = screen_row + row_offset;
+
+        if (file_row >= editor.countRows()) {
+          mvaddch(screen_row, 0, '~');
+        } else {
+          std::string rendered = editor.getRow(file_row).getRendered();
+          std::string visible_rendered = rendered.substr(
+              column_offset, column_offset + COLS);
+          mvaddstr(screen_row, 0, visible_rendered.c_str());
         }
       }
-      break;
-    case KEY_HOME:
-      // Move to the first column
-      state.cursor_column = 0;
-      break;
-    case KEY_END:
-      // Move to the right of the screen
-      if (state.cursor_row < state.rows.size()) {
-        state.cursor_column = state.rows[state.cursor_row].size();
-      }
-      break;
-  }
+    }
 
-  if (state.cursor_row > state.rows.size()) {
-    state.cursor_column = 0;
-  } else {
-    int row_length = state.rows[state.cursor_row].size();
-    if (state.cursor_column > row_length) state.cursor_column = row_length;
-  }
-}
+    void drawStatus() {
+      attron(A_STANDOUT);
 
-void processKey() {
-  int key = getch();
-  switch (key) {
-    case CTRL_KEY('q'):
-      endwin();
-      exit(0);
-      break;
+      // Fill with spaces to create background
+      int j;
+      for (j = 0; j < COLS; j++) mvaddch(LINES-1, j, ' ');
 
-    case KEY_UP:
-    case KEY_DOWN:
-    case KEY_LEFT:
-    case KEY_RIGHT:
-    case KEY_HOME:
-    case KEY_END:
-      moveCursor(key);
-      break;
+      // Overwrite some of the spaces with the status
+      mvprintw(LINES-1, 0, "%.40s - %d lines",
+        editor.isFileOpen() ? editor.getFilename().c_str() : "[no name]",
+        editor.countRows()
+      );
 
-    case '\r':
-      insertNewline();
-      break;
+      attroff(A_STANDOUT);
+    }
 
-    case KEY_BACKSPACE:
-    case KEY_DC:
-    case CTRL_KEY('h'):
-      if (key == KEY_DC) moveCursor(KEY_RIGHT);
-      deleteCharacter();
-      break;
+    void refreshScreen() {
+      clear();
+      lines = LINES - 1;
+      clampScroll();
+      drawRows();
+      drawStatus();
+      move(
+        editor.getCursor().getRow() - row_offset,
+        editor.getRenderedColumn() - column_offset
+      );
+      refresh();
+    }
 
-    default:
-      insertCharacter(key);
-      break;
-  }
-}
-
-/*** main ***/
+    int getKey() {
+      return getch();
+    }
+};
 
 int main(int argc, char *argv[]) {
-  initialiseState();
-  initialiseScreen();
+  Editor editor;
+  Display display(editor);
+  display.initialiseScreen();
 
   if (argc >= 2) {
-    openFile(argv[1]);
+    editor.openFile(argv[1]);
   }
 
   while(1) {
-    refreshScreen();
-    processKey();
+    display.refreshScreen();
+    editor.processKey(display.getKey());
   }
 
   return 0;
