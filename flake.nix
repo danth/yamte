550d674a9a322cf647b6c3afc8562c929c039004
Daniel Thwaites <danthwaites30@btinternet.com>
2021-07-27 14:14:45 +0100

    Provide debug package :hammer:

--- flake.nix
+++ flake.nix
@@ -8,12 +8,22 @@
     utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages.yamte = pkgs.stdenv.mkDerivation {
-          name = "yamte";
-          src = ./.;
-          buildInputs = with pkgs; [ ncurses ];
-          buildPhase = "g++ src/*.cpp -o yamte -Wall -Wextra -pedantic -lncurses";
-          installPhase = "install -D yamte $out/bin/yamte";
+        packages = {
+          yamte = pkgs.stdenv.mkDerivation {
+            name = "yamte";
+            src = ./.;
+            buildInputs = with pkgs; [ ncurses ];
+            buildPhase = "g++ src/*.cpp -o yamte -Wall -Wextra -pedantic -lncurses";
+            installPhase = "install -D yamte $out/bin/yamte";
+          };
+          yamte-debug = pkgs.stdenv.mkDerivation {
+            name = "yamte-debug";
+            src = ./.;
+            buildInputs = with pkgs; [ ncurses ];
+            buildPhase = "g++ src/*.cpp -o yamte -ggdb -O0 -Wall -Wextra -pedantic -lncurses";
+            installPhase = "install -D yamte $out/bin/yamte";
+            dontStrip = true;
+          };
         };
         defaultPackage = packages.yamte;
 
