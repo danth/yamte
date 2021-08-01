3e76e433970f0a83aff5cf0602ae80fcbb188f23
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-01 23:13:17 +0100

    Change line when moving left/right at line ends :sparkles:
    This was present in the C++ implementation so I have added it in Haskell
    for consistency.

--- app/Yamte/Cursor.hs
+++ app/Yamte/Cursor.hs
@@ -12,11 +12,14 @@ module Yamte.Cursor (
 import qualified Data.Sequence as S
 import Yamte.Editor (State(..), Buffer, Cursor)
 
-rowLength :: Int -> Buffer -> Int
+rowLength :: Buffer -> Int -> Int
 -- The cursor is allowed to move one row below the end of the buffer
-rowLength row buffer = if row < length buffer
+rowLength buffer row = if row < length buffer
                           then length $ buffer `S.index` row
                           else 0
+
+rowLength' :: State -> Int -> Int
+rowLength' state = rowLength $ stateBuffer state
 
 clamp :: Ord a => a -> a -> a -> a
 clamp min max value = if value < min
@@ -28,44 +31,45 @@ clamp min max value = if value < min
 clampCursor :: Buffer -> Cursor -> Cursor
 clampCursor buffer (row, column) =
   let row' = clamp 0 (length buffer) row
-   in if row' < length buffer
-      then let column' = clamp 0 (rowLength row' buffer) column
-            in (row', column')
-      else (row', 0)
+      column' = clamp 0 (rowLength buffer row') column
+   in (row', column')
 
-moveBy :: (Int, Int) -> State -> State
-moveBy (rowOffset, columnOffset) state =
-  let (row, column) = stateCursor state
-      row' = row + rowOffset
-      column' = column + columnOffset
-      cursor = clampCursor (stateBuffer state) (row', column')
-   in state { stateCursor = cursor }
+clampCursor' :: State -> Cursor -> Cursor
+clampCursor' state = clampCursor $ stateBuffer state
+
+move :: State -> (Cursor -> Cursor) -> State
+move state f =
+  let newCursor = f $ stateCursor state
+      newCursor' = clampCursor' state newCursor
+   in state { stateCursor = newCursor' }
 
 moveLeft :: State -> State
-moveLeft = moveBy (0, -1)
+moveLeft state = move state $ \(row, column) ->
+  if column > 0
+     then (row, column - 1)
+     else (row - 1, maxBound)
 
 moveRight :: State -> State
-moveRight = moveBy (0, 1)
+moveRight state = move state $ \(row, column) ->
+  let columns = rowLength' state row
+   in if column < columns
+         then (row, column + 1)
+         else (row + 1, 0)
 
 moveUp :: State -> State
-moveUp = moveBy (-1, 0)
+moveUp state = move state $ \(row, column) -> (row - 1, column)
 
 moveDown :: State -> State
-moveDown = moveBy (1, 0)
-
-setCursor :: (Cursor -> Buffer -> Cursor) -> State -> State
-setCursor f state =
-  let newCursor = f (stateCursor state) (stateBuffer state)
-   in state { stateCursor = clampCursor (stateBuffer state) newCursor }
+moveDown state = move state $ \(row, column) -> (row + 1, column)
 
 moveTop :: State -> State
-moveTop = setCursor (\(row, column) buffer -> (0, column))
+moveTop state = move state $ \(row, column) -> (0, column)
 
 moveBottom :: State -> State
-moveBottom = setCursor (\(row, column) buffer -> (length buffer, column))
+moveBottom state = move state $ \(row, column) -> (maxBound, column)
 
 moveHome :: State -> State
-moveHome = setCursor (\(row, column) buffer -> (row, 0))
+moveHome state = move state $ \(row, column) -> (row, 0)
 
 moveEnd :: State -> State
-moveEnd = setCursor (\(row, column) buffer -> (row, rowLength row buffer))
+moveEnd state = move state $ \(row, column) -> (row, maxBound)
