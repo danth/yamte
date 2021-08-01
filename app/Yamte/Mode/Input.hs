1f3d9a1363d4f3bdee0ad5f15b5c2efa8f682b1f
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-01 16:52:39 +0100

    Enable arrow keys in input mode :bug:

--- app/Yamte/Mode/Input.hs
+++ app/Yamte/Mode/Input.hs
@@ -1,6 +1,14 @@
 module Yamte.Mode.Input (inputMode) where
 
 import Yamte.Editor (Mode(Mode), Action(Action))
+import Yamte.Cursor
+import UI.NCurses (Key(..))
 
 inputMode :: Mode
-inputMode = Mode "Input" []
+inputMode = Mode "Input" [ (Action (Right KeyLeftArrow) moveLeft)
+                         , (Action (Right KeyRightArrow) moveRight)
+                         , (Action (Right KeyUpArrow) moveUp)
+                         , (Action (Right KeyDownArrow) moveDown)
+                         , (Action (Right KeyHome) moveHome)
+                         , (Action (Right KeyEnd) moveEnd)
+												 ]
