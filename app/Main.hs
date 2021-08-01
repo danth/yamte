f61308309aada1e1e856c138c44cd88839113d7d
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-01 01:19:44 +0100

    Create skeleton UI :lipstick:

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,64 @@ module Main where
 
 import UI.NCurses
 
+data Windows  = Windows { statusWindow :: Window
+                        , sidebarWindow :: Window
+                        , bufferWindow :: Window
+                        , messageWindow :: Window
+                        }
+
+invertBackground :: Update ()
+invertBackground = setBackground $ Glyph ' ' [AttributeReverse]
+
+createWindows :: Curses Windows
+createWindows = do
+    (rows, columns) <- screenSize
+
+    status <- newWindow 1 columns 0 0
+    updateWindow status invertBackground
+
+    sidebar <- newWindow (rows-2) 4 1 0
+    updateWindow sidebar invertBackground
+
+    buffer <- newWindow (rows-2) (columns-4) 1 4
+
+    message <- newWindow 1 columns (rows-1) 0
+    updateWindow message invertBackground
+
+    return Windows { statusWindow = status
+                   , sidebarWindow = sidebar
+                   , bufferWindow = buffer
+                   , messageWindow = message
+                   }
+
+draw :: Windows -> Curses ()
+draw windows = do
+    updateWindow (statusWindow windows) $ do
+        clear
+
+    updateWindow (sidebarWindow windows) $ do
+        clear
+
+    updateWindow (bufferWindow windows) $ do
+        clear
+
+    updateWindow (messageWindow windows) $ do
+        clear
+        moveCursor 0 0
+        drawString "Welcome to Yamte!"
+
+    updateWindow (bufferWindow windows) $ moveCursor 0 0
+
+    render
+
 main :: IO ()
 main = runCurses $ do
     setEcho False
-    w <- defaultWindow
-    updateWindow w $ do
-        moveCursor 1 10
-        drawString "Hello world!"
-        moveCursor 3 10
-        drawString "(press q to quit)"
-        moveCursor 0 0
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+
+    windows <- createWindows
+    draw windows
+
+    waitFor (bufferWindow windows) (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
 
 waitFor :: Window -> (Event -> Bool) -> Curses ()
 waitFor w p = loop where
