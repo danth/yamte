1e9bb2ce5b807e610929278d5f9ed0fce82997b9
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-03 15:55:19 +0100

    Exit file mode after choosing an action :sparkles:

--- app/Yamte/Mode/File.hs
+++ app/Yamte/Mode/File.hs
@@ -2,14 +2,19 @@ module Yamte.Mode.File
   ( fileMode
   ) where
 
-import Yamte.Editor (Mode, leaveMode, reloadFile, saveFile)
+import Yamte.Editor (Mode, State, leaveMode, reloadFile, saveFile)
 import Yamte.Mode.GenericAction
+
+leaveAfter :: (State -> IO State) -> State -> IO State
+leaveAfter operation state = do
+  state' <- operation state
+  return $ leaveMode state'
 
 actions :: [Action]
 actions =
   [ (Action (Left '\^Q') leaveMode)
-  , (IOAction (Left 's') saveFile)
-  , (IOAction (Left 'r') reloadFile)
+  , (IOAction (Left 's') $ leaveAfter saveFile)
+  , (IOAction (Left 'r') $ leaveAfter reloadFile)
   ]
 
 fileMode :: Mode
