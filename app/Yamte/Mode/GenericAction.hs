1d9d423df6f81244d2a7ffe7734690401147c96c
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-08 09:09:54 +0100

    Show available actions by pressing ? :sparkles:
    Descriptions need to be shown to finish #15.

--- app/Yamte/Mode/GenericAction.hs
+++ app/Yamte/Mode/GenericAction.hs
@@ -3,12 +3,26 @@ module Yamte.Mode.GenericAction
   , makeActionMode
   ) where
 
-import Data.List (find)
-import Yamte.Editor (Mode(Mode), ModeResponse(..), State, Trigger)
+import Data.List (find, intercalate)
+import Yamte.Editor (Mode(Mode), ModeResponse(..), State(..), Trigger)
+import UI.NCurses (Key(..))
 
 data Action
   = Action Trigger (State -> State)
   | IOAction Trigger (State -> IO State)
+
+showKey :: Key -> String
+showKey KeyUpArrow = "↑"
+showKey KeyDownArrow = "↓"
+showKey KeyLeftArrow = "←"
+showKey KeyRightArrow = "→"
+showKey key = drop 3 $ show key
+
+instance Show Action where
+  show (Action (Left char) _) = [char]
+  show (Action (Right key) _) = showKey key
+  show (IOAction (Left char) _) = [char]
+  show (IOAction (Right key) _) = showKey key
 
 getTrigger :: Action -> Trigger
 getTrigger (Action trigger _) = trigger
@@ -27,5 +41,15 @@ handleTrigger actions trigger state =
       state' <- f state
       return $ NewState state'
 
+showHints :: [Action] -> State -> State
+showHints actions state =
+  let message = intercalate " " $ map show actions
+   in state {stateMessage = message}
+
+makeHintAction :: [Action] -> Action
+makeHintAction actions = Action (Left '?') $ showHints actions
+
 makeActionMode :: String -> [Action] -> Mode
-makeActionMode name actions = Mode name $ handleTrigger actions
+makeActionMode name actions =
+  let actions' = (makeHintAction actions):actions
+   in Mode name $ handleTrigger actions'
