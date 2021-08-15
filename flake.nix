c9cd5ec4e559847886402c7097071cf6a87598f9
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-15 11:56:36 +0100

    Build using haskell.nix
    Needed to get base >= 4.15.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    haskell.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskell/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -8,26 +9,30 @@
     };
   };
 
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachSystem ["x86_64-linux" "i686-linux" "aarch64-linux"]
+  outputs = { haskell, nixpkgs, utils, ... }:
+    # Cannot support i686-linux as haskell.nix does not support it
+    # Cannot support aarch64-linux due to input-output-hk/haskell.nix#1189
+    utils.lib.eachSystem ["x86_64-linux"]
     (system:
-      with nixpkgs.lib;
-      with nixpkgs.legacyPackages.${system}.haskellPackages;
-      rec {
-        packages = {
-          yamte = mkDerivation {
-            pname = "yamte";
-            version = "0.1.0.0";
-            src = ./.;
-            isLibrary = false;
-            isExecutable = true;
-            executableHaskellDepends = [ base ilist ncurses skylighting ];
-            license = "AGPL-3.0";
-          };
-        };
-        defaultPackage = packages.yamte;
-
-        apps.yamte = utils.lib.mkApp { drv = packages.yamte; };
-        defaultApp = apps.yamte;
+      let
+        overlays = [
+          haskell.overlay
+          (final: prev: {
+            yamte = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc901";
+              modules = [{
+                packages.ncurses.components.library.libs =
+                  final.lib.mkForce [ final.ncurses ];
+              }];
+            };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        flake = pkgs.yamte.flake {};
+      in rec {
+        inherit (flake) packages apps;
+        defaultPackage = packages."yamte:exe:yamte";
+        defaultApp = apps."yamte:exe:yamte";
       });
 }
